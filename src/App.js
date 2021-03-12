f98f666fd31fafa79000b41a9e9c813e8c837a58
Jacob Walton <jawalton6616@gmail.com>
2021-03-12 09:03:17 -0600

    styled header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <header className="header">React-NASA</header>
+    </div>
+  );
 }
 
 export default App;
