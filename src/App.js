3d96657643da42fbe0ffb83a1d06e7163dddcd14
Jacob Walton <jawalton6616@gmail.com>
2021-03-12 09:28:19 -0600

    adding modal and media queries

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import PhotoOfTheDay from "./Components/PhotoOfTheDay/PhotoOfTheDay";
 
 function App() {
   return (
     <div className="App">
       <header className="header">React-NASA</header>
+      <PhotoOfTheDay />
     </div>
   );
 }
---
+++ src/Components/PhotoOfTheDay/PhotoOfTheDay.js
@@ -0,0 +1,68 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Modal from "@material-ui/core/Modal";
+import Backdrop from "@material-ui/core/Backdrop";
+import Fade from "@material-ui/core/Fade";
+import "./photo-of-the-day.css";
+
+const useStyles = makeStyles((theme) => ({
+  modal: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    backdropFilter: "blur(5px)",
+  },
+  paper: {
+    backgroundColor: theme.palette.background.paper,
+    border: "2px solid #000",
+    borderRadius: "100px 20px 30px 10px",
+    height: "fit-content",
+    width: "fit-content",
+    padding: "30px",
+    outline: "none",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
+
+export default function TransitionsModal() {
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <button className="photo-btn" type="button" onClick={handleOpen}>
+        Photo Of The Day
+      </button>
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        className={classes.modal}
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={open}>
+          <div className={classes.paper}>
+            <h2 id="transition-modal-title">Transition modal</h2>
+            <p id="transition-modal-description">
+              react-transition-group animates me.
+            </p>
+          </div>
+        </Fade>
+      </Modal>
+    </div>
+  );
+}
